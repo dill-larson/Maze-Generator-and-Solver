632c226baef36310cdb0f8afb546e4a501864799
Dillon Larson <61177185+dill-larson@users.noreply.github.com>
2020-04-22 12:43:25 -0700

    Add files via upload
    Update Maze constructor to fill-out maze

--- Maze.java
+++ Maze.java
@@ -7,13 +7,19 @@ public class Maze {
     public Maze(int size) {
         maze = new Graph<>();
         this.size = size;
-        int spacing = size % 2;
+        int spacing = (int)Math.floor(size/2);
+        int idk = size - 1;
+
         for(int i = 0; i < size*size; ++i) { //rows
-            for(int j = i; j < i + size; ++j) { //columns
-                if((i == size - 1) && ((j / (size - 1)) != 1)) {
-                    maze.addEdge(i, j + 1, true);
+            for(int j = i + 1; j < size*size; j += spacing + 1) { //columns
+                if(j <= i + size) {
+                    if ((i == idk) && (j % size == 0)) {
+                        idk += size;
+                        continue;
+                    } else {
+                        maze.addEdge(i, j, true);
+                    }
                 }
-                j += spacing;
             }
         }
     }
